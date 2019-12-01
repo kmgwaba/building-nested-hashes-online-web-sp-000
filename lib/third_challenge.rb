d95414e4f52c1f10543e76ffe05787723c206ed3
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 14:50:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -29,7 +29,7 @@ def third_challenge
         name : "Juliet", age : "15", status : "alive"
         #your key/value pairs here
         },
-      :heroine_friends => [{name : "Steven", age : "30", attitude : "confused" },
+      :heroine_friends => [ {name : "Steven", age : "30", attitude : "confused"},
       {name : "Nurse", age : "44", attitude : "worried"}
     ]
    }
