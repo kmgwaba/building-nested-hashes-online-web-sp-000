6476bac75783ce032791c64c691bcace264055b3
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 14:48:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,15 +2,15 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague", :age => "53"
+        name : "Lord Montague", age : "53"
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lady Montague", :age => "54"
+        name : "Lady Montague", age : "54"
         #your key/value pairs here
         },
       :hero => {
-        :name => "Romeo", :age => 15, :status => "alive"
+        name : "Romeo", age : "15", status : "alive"
         #your key/value pairs here
         },
       :hero_friends => [
@@ -19,16 +19,14 @@ def third_challenge
     ]
    },
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet", :age => "50"
+      :patriarch => {name : "Lord Capulet", age : "50"},
         #your key/value pairs here
-        },
-      :matriarch => {
-        :name => "Lady Capulet", :age => "51"
+      
+      :matriarch => {name : "Lady Capulet", age : "51"
         #your key/value pairs here
-        },
+      
       :heroine => {
-        :name => "Juliet", :age => "15", :status => "alive"
+        name : "Juliet", age : "15", status : "alive"
         #your key/value pairs here
         },
       :heroine_friends => [{name : "Steven", age : "30", attitude : "confused" },
