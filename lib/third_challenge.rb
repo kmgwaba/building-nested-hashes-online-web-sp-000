d0e447f7c478003d6ee86b26d250cba01a2e6f3b
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 15:12:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,10 +13,8 @@ def third_challenge
         name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Mercutio", age: "18", attitude: "hot-headed"}
-    ]
+      :hero_friends => [] 
+
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -28,10 +26,8 @@ def third_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
         #your key/value pairs here
 
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-      {name: "Nurse", age: "44", attitude: "worried"}
-    ]
+      :heroine_friends => []
+
   }
 }
 
