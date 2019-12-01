1f956755f373d77e416380093f154e79cadd8097
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-01 14:53:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -31,8 +31,10 @@ def third_challenge
       :heroine_friends => [ {name : "Steven", age : "30", attitude : "confused"},
       {name : "Nurse", age : "44", attitude : "worried"}
     ]
-   }
-  
+  }
+} 
+
+
 
 
 end
